73dbb1134fcb6cba5a69a24e8fbc094cdd34323f
Damian AR <damitech@tutamail.com>
2025-06-30 12:38:25 +0200

    Creating page

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-  base: "./", // Rutas relativas para GitHub Pages
+  base: process.env.NODE_ENV === 'production' ? '/portfolio_project/' : '/',
+
+  build: {
+    outDir: 'dist', // Carpeta donde se generarán los archivos de producción
+    rollupOptions: {
+      input: 'index.html'
+    },
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
